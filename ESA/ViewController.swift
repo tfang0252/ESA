f918f7f99c879d543b3c8870dcc571655c1487e5
Zachary Frederick <zfrederick6856@eagle.fgcu.edu>
2018-07-12 13:49:18 -0400

    hi

--- ESA/ViewController.swift
+++ ESA/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hi")
+        print("bye")
     }
 
     override func didReceiveMemoryWarning() {
