437b3e97d8ba77f7ff51fc22328925d70df7141b
tfang0252 <32147688+tfang0252@users.noreply.github.com>
2018-10-27 23:11:46 -0400

    fixed drag and drop
    drag and drop works a lot better now

---
+++ ESA/FormationCell.swift
@@ -0,0 +1,15 @@
+//
+//  FormationCell.swift
+//  ESA
+//
+//  Created by Tony Fang on 10/27/18.
+//  Copyright © 2018 Tony Fang. All rights reserved.
+//
+
+import UIKit
+
+class FormationCell: UICollectionViewCell {
+    
+    @IBOutlet weak var playerImage: UIImageView!
+    @IBOutlet weak var playerButton: UIButton!
+}
--- ESA/FormationVC.swift
+++ ESA/FormationVC.swift
@@ -11,11 +11,8 @@ import FirebaseDatabase
 
 class FormationVC: UIViewController,UIDropInteractionDelegate{
     
+    @IBOutlet weak var FormationCV: UICollectionView!
     
-    
-    @IBOutlet weak var myUIView: UIView!
-    
-    let items = ["tony","juan","zach","danny","dani"]
     
     var ref: DatabaseReference!
     
@@ -24,30 +21,10 @@ class FormationVC: UIViewController,UIDropInteractionDelegate{
     var playerName = ""
     var playerNumber = ""
     
-    var names = [String]()
-    var nums = [String]()
-    
-    private lazy var button: UIButton = {
-        let minimumTappableHeight: CGFloat = 44
-        let button = UIButton(frame: CGRect(x: 0, y: 0,
-                                            width: minimumTappableHeight,
-                                            height: minimumTappableHeight))
-        button.center = self.view.center
-        button.layer.cornerRadius = minimumTappableHeight / 2
-        button.layer.masksToBounds = true
-        button.backgroundColor = UIColor.yellow
-        //let testImage = UIImage(named: "LoginBG.png")
-        //button.setBackgroundImage(testImage, for: .normal)
-        button.setTitle("❌", for: .normal)
-        button.setTitle("⭕️", for: .highlighted)
-        
-        return button
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        
+        view.addInteraction(UIDropInteraction(delegate: self))
+        FormationCV.dragInteractionEnabled = true
         ref = Database.database().reference().child("Player")
         
         //When a new player is added to the database, it is observed and then the player model is added to the playerNames array
@@ -66,59 +43,123 @@ class FormationVC: UIViewController,UIDropInteractionDelegate{
                     let player = PlayerModel(PlayerName: playerName as! String?, PlayerNumber: playerNumber as! String?)
                     
                     self.playerNames.append(player)
-                    self.names.append(player.PlayerName!)
-                    self.nums.append(player.PlayerNumber!)
                 }
+                
+                self.FormationCV.reloadData()
+            }
+            
+        })
+      
+    }
+    
+  
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        // Ensure the drop session has an object of the appropriate type
+        return session.canLoadObjects(ofClass: UIImage.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session:UIDropSession)->UIDropProposal{
+        return UIDropProposal(operation: .move)
+    }
+    
+    func dropInteraction(_ interacton: UIDropInteraction, performDrop session: UIDropSession){
+        for dragItem in session.items{
+            dragItem.itemProvider.loadObject(ofClass: UIImage.self,completionHandler: {(obj, err) in
+            if let err = err{
+                print("Failed to load our dragged item:", err)
+                return
+            }
+            guard let draggedImage = obj as? UIImage else
+            {return}
+            DispatchQueue.main.async{
+                let imageView = UIImageView(image: draggedImage)
+                self.view.addSubview(imageView)
+                imageView.frame = CGRect(x:0, y: 0, width: 50, height: 50)
+                
+                let centerPoint = session.location(in: self.view)
+                imageView.center = centerPoint
             }
         })
-        print(names)
-        
-        createButtons()
-        
-        
-        //myUIView.addSubview(button)
+    
+    }
     }
     
-    @objc func drag(control: UIControl, event: UIEvent) {
-        if let center = event.allTouches?.first?.location(in: self.myUIView) {
-            control.center = center
-        }
-    }
-    
-    func createButtons(){
-        var buttonX: CGFloat = 20
-        var buttonY: CGFloat = 625
-        for name in items{
-           
-            let playerButton = UIButton(frame: CGRect(x: buttonX, y: buttonY,
-                                                width: 45,
-                                                height: 45))
-            buttonX = buttonX+55
-            
-            playerButton.layer.masksToBounds = true
-            
-            playerButton.backgroundColor = UIColor.yellow
-            playerButton.setTitle(name, for: .normal)
-            playerButton.setTitleColor(.red, for: .normal)
-            //let testImage = UIImage(named: "LoginBG.png")
-            //button.setBackgroundImage(testImage, for: .normal)
-            playerButton.layer.cornerRadius = 22
-            //playerButton.setTitle("❌", for: .normal)
-           // playerButton.setTitle("⭕️", for: .highlighted)
-            playerButton.addTarget(self,
-                             action: #selector(drag(control:event:)),
-                             for: UIControlEvents.touchDragInside)
-            playerButton.addTarget(self,
-                             action: #selector(drag(control:event:)),
-                             for: [UIControlEvents.touchDragExit,
-                                   UIControlEvents.touchDragOutside])
-            myUIView.addSubview(playerButton)
-        }
+    func textToImage(drawText: NSString, inImage: UIImage, atPoint:CGPoint)->UIImage{
         
+        // Setup the font specific variables
+        var textColor: UIColor = UIColor.black
+        var textFont: UIFont = UIFont(name: "Helvetica Bold", size: 15)!
+        
+        //Setup the image context using the passed image.
+        UIGraphicsBeginImageContext(inImage.size)
+        
+        //Setups up the font attributes that will be later used to dictate how the text should be drawn
+        let textFontAttributes = [
+            NSAttributedStringKey.font: textFont,
+            NSAttributedStringKey.foregroundColor: textColor,
+            ]
+        
+        //Put the image into a rectangle as large as the original image.
+        inImage.draw(in: CGRect(x:0, y:0, width: inImage.size.width, height: inImage.size.height))
+        
+        // Creating a point within the space that is as bit as the image.
+        var rect: CGRect = CGRect(x:atPoint.x, y:atPoint.y, width:inImage.size.width, height:inImage.size.height)
+        
+        //Now Draw the text into an image.
+        drawText.draw(in: rect, withAttributes: textFontAttributes)
+        
+        // Create a new image out of the images we have created
+        var newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        
+        // End the context now that we have the image we need
+        UIGraphicsEndImageContext()
+        
+        //And pass it back up to the caller.
+        return newImage
+        
+    }
 
-        
+}
+
+extension FormationVC: UICollectionViewDelegate,UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection: Int) ->Int{
+        return playerNames.count
     }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)as! FormationCell
+        
+        
+        let players: PlayerModel
+        
+        players = playerNames[indexPath.item]
+        
+        cell.playerImage.image = textToImage(drawText: players.PlayerName! as NSString, inImage: UIImage(named: "formation.png")!, atPoint: CGPoint(x: 12, y: 18))
+        //cell.playerButton.setTitle(players.PlayerName, for: .normal)
+        
+        
+        return cell
+    }
+}
+
+extension FormationVC: UICollectionViewDragDelegate{
+
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)as! FormationCell
+        let players: PlayerModel
+        players = playerNames[indexPath.item]
+        
+        cell.playerImage.image = textToImage(drawText: players.PlayerName! as NSString, inImage: UIImage(named: "formation.png")!, atPoint: CGPoint(x: 12, y: 18))
+        
+        guard let image = cell.playerImage.image else { return [] }
+
+        let itemProvider = NSItemProvider(object: image)
+        let dragPlayer = UIDragItem(itemProvider: itemProvider)
+        dragPlayer.localObject = image
+        return [dragPlayer]
+    }
 }
 
 
